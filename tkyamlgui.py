bdefbbfab6848c253b9e32b6983a1e651eedd76e
Lawrence Cheung <lcheung@skybridge-login2.sandia.gov>
2021-07-13 17:29:46 -0600

    Added mouse-over tool tip capability

--- tkyamlgui.py
+++ tkyamlgui.py
@@ -138,6 +138,51 @@ def tkextractval(inputtype, tkvar, tkentry, optionlist=[]):
         val = float(tkentry.get())
     return val
 
+class ToolTip(object):
+    """
+    Creates a mouse-over tool tip show additional context
+    """
+    # See https://stackoverflow.com/questions/20399243/display-message-when-hovering-over-something-with-mouse-cursor-in-python
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 57
+        y = y + cy + self.widget.winfo_rooty() +27
+        self.tipwindow = tw = Tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        label = Tk.Label(tw, text=self.text, justify=Tk.LEFT,
+                         background="#ffffe0", relief=Tk.SOLID, borderwidth=1,
+                         font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+
+def CreateToolTip(widget, text):
+    """
+    Binds a ToolTip to widget with text
+    """
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
 class inputwidget:
     """
     Creates a general-purpose widget for input 
@@ -178,6 +223,9 @@ class inputwidget:
             else:            
                 self.tklabel.grid(row=row, columnspan=cspan,
                                   column=0, sticky='nw', padx=5)
+            if 'help' in self.outputdef:
+                CreateToolTip(self.tklabel, text=self.outputdef['help'])
+
         if labelonly: return None
 
         if inputtype is bool:
@@ -1012,6 +1060,8 @@ class App(Tk.Tk, object):
                     b.grid(row=button['row'], column=col, padx=5, sticky='w')
                 else:
                     b.grid(column=col, padx=5, sticky='w')
+                if 'help' in button:
+                    CreateToolTip(b, button['help'])
 
         # link any widgets necessary
         for key,  inputvar in self.inputvars.items():
