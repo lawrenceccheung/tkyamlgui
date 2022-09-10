d3a9d0543e5a8367d3a9c11980b33aeb7d0e902c
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2022-09-10 14:06:47 -0600

    Added onoffctrlelem for string inputs

--- tkyamlgui.py
+++ tkyamlgui.py
@@ -704,6 +704,30 @@ class inputwidget:
                     else:
                         try: elem['ctrlinput'].tkentry.config(state=inputstate)
                         except: None
+        # Handle the str type
+        if self.inputtype == str:
+            for ielem, elem in enumerate(self.ctrlelem):
+                criteria  = elem['activewhen']
+                optiontest= criteria[0]
+                condition = criteria[1]
+                if currstate==condition:
+                    framestate, inputstate = 'normal', 'normal'
+                else:
+                    framestate, inputstate = 'disable', 'disabled'
+                if elem['ctrlframe'] is not None:
+                    #print("Set "+elem['frame']+" to "+framestate)
+                    for child in elem['ctrlframe'].winfo_children():
+                        try:    child.configure(state=framestate)
+                        except: None
+                if elem['ctrlinput'] is not None:
+                    #print("Set "+elem['input']+" to "+inputstate)
+                    if isinstance(elem['ctrlinput'].tkentry, list):
+                        for entry in elem['ctrlinput'].tkentry:
+                            try:    entry.config(state=inputstate)
+                            except: None
+                    else:
+                        try: elem['ctrlinput'].tkentry.config(state=inputstate)
+                        except: None
         # Handle the listbox option 
         if self.inputtype == moretypes.listbox:
             # Get the current state
